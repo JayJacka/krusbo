39e12f150e918b085389b443e7e8d316fe5260fd
bpremika <premika.yu@gmail.com>
2024-04-05 14:16:11 +0700

    feat: select color from index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,10 @@ export default async function Home() {
 		<main className="flex min-h-screen flex-col items-center justify-center bg-black text-white gap-4 text-4xl">
 			Hello, {user?.username}!
 			<UserButton />
-			<UserCard name="Bow"/>
+			<UserCard name="Bow" index={0}/>
+			<UserCard name="Bow" index={1}/>
+			<UserCard name="Bow" index={2}/>
+			<UserCard name="Bow" index={3}/>
 		</main>
 	);
 }
