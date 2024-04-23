56f0b761847dcd57dd1beb353b7efc9be94820be
Supawit Saeliew <63398464+JayJacka@users.noreply.github.com>
2024-04-23 19:43:03 +0700

    fix: socket creation

--- src/socket.ts
+++ src/socket.ts
@@ -1,19 +1,18 @@
 "use client";
 
 import { io } from "socket.io-client";
-let socket
+let socket;
 
 if (typeof window !== "undefined") {
 	socket = io("http://localhost:3000", {
 		query: {
 			name: localStorage.getItem("name"),
 		},
-	})
-	console.log("socket", socket)
-	socket.auth = {
-		sessionID: localStorage.getItem("sessionID"),
-		userID: localStorage.getItem("userID"),
-	};
+		auth: {
+			sessionID: localStorage.getItem("sessionID"),
+			userID: localStorage.getItem("userID"),
+		},
+	});
 } else {
 	socket = io();
 }
